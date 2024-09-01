bd61f3dd61083e59f73de3db8eb5fda928c27b4e
Paintballskaguy <Johnstournament@gmail.com>
2024-09-01 21:58:55 +0000

    task 6 - matrix printing of ints. added a space in the first print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, elem in enumerate(row):
             if i < len(row) - 1:
-                print("{:d}".format(elem), end="")
+                print("{:d}".format(elem), end=" ")
             else:
                 print("{:d}".format(elem), end="")
         print("")
