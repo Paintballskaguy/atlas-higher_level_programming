fab5b8e08773feac935801fea1807bdc20296063
Paintballskaguy <Johnstournament@gmail.com>
2024-09-04 01:39:26 +0000

    task 12- i failed to not use google

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not an int or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     def roman_number = {
