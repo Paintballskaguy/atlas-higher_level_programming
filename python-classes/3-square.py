d44c404f4666088f1d3111fc0cab8087fe292fa8
Paintballskaguy <Johnstournament@gmail.com>
2024-09-09 01:59:29 +0000

    task 3 - empty classes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -22,4 +22,15 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueEr
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """
+            Calculates the area of the square.
+            Returns:
+                int: The area of the square.
+            """
+            return self.__size ** 2
