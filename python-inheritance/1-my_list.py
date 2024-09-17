f9ef1fe3194d2c220a1f7142d9d000b163621290
Paintballskaguy <Johnstournament@gmail.com>
2024-09-16 23:12:43 -0500

    formatting again.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,12 +7,12 @@ MyList is a public instance method `print_sorted` prints in ascending order.
 
 
 class MyList(list):
-    """Inherits from the built-in list class and prints a sorted version of the list."""
+    """Inherits from the list and prints a sorted version of the list."""
 
     def __init__(self):
         """Initializes the object (optional)."""
         super().__init__()
 
     def print_sorted(self):
-        """Prints the list, sorted in ascending order (without changing original list)."""
+        """Prints the list, in ascending order (without changing original list)."""
         print(sorted(self))
