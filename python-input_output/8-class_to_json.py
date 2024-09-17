fd53f09a9162f2c53b79d42607cb16354e8a2dfa
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 13:34:46 -0500

    task 8 - classy json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function to return the dictionary description
+for JSON of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: The dictionary description of the object's attributes.
+    """
+    
+    return obj.__dict__
