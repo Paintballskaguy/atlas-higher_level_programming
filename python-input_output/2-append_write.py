8ac5d5c02b5b0490a46855b751bee38b971a7e41
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 11:31:37 -0500

    task 2 - adding stuff to the end of a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines the function to add to the end of
+a text file in UTF8 and return number
+of characters that was added
+"""
+
+
+def append_write(filename="", text=""):
+	"""
+	appends a string to the end of a text file.
+	
+	    Args:
+        filename (str): The name of the file to append to.
+        text (str): The text to append to the file.
+
+    Returns:
+        int: The number of characters added to the file.
+    """
+	with open(filename, 'a', encoding='utf-8') as file:
+		return file.write(text)
