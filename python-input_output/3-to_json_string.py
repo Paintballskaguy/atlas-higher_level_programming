66805fac32ae3e6f374d9138b5f8596a3d21a0ad
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 11:54:45 -0500

    formatting

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -7,13 +7,14 @@ an object
 
 import json
 
+
 def to_json_string(my_obj):
     """
     Returns the JSON of an object (str)
 
     Args:
         my_obj: the object to convert to JSON
-  
+
     Returns:
         str: the JSON stuff of the object.
     """
