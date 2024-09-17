97fcd3b8a2f67de87c0a7928fc04edd2989d0c57
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 13:20:05 -0500

    forgot to add return

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -18,4 +18,4 @@ def load_from_json_file(filename):
         object: The Python object created from the JSON file.
     """
     with open(filename, 'r', encoding='utf-8') as file:
-        json.load(file)
+        return json.load(file)
