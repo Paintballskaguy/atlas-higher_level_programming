6457d2e41547ca63fc65c51ebefbd79011b9bf26
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 11:10:24 -0500

    formating

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-	"""
-	Defines a function to read UTF8 text and print out contents.
-	"""
+"""
+Defines a function to read UTF8 text and print out contents.
+"""
  
- def read_file(filename=""):
+def read_file(filename=""):
     """Reads the file and prints out contents"""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
