ee5861748238d7c77a8648cd3db5ac498241b21e
Paintballskaguy <Johnstournament@gmail.com>
2024-09-16 22:38:32 -0500

    formatting

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,7 @@
 This module defines the function is_kind_of_class.
 
 The function returns True if the object is an instance of, or if the object is
-an instance of a class that inherited from, the specified class; otherwise False.
+an instance of a class that inherited from, the class; otherwise False.
 """
 
 
