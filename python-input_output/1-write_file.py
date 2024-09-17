46116f250acd144393ca07acd12da3713685479b
Paintballskaguy <Johnstournament@gmail.com>
2024-09-17 11:20:46 -0500

    formatting again

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,14 +8,15 @@ characters written
 
 def write_file(filename="", text=""):
 	"""
-    Writes a string to a UTF8 text file and returns the number of characters written.
+    Writes a string to a UTF8 text file and returns the
+    number of characters written.
 
     Args:
-        filename (str): The name of the file to write to.
-        text (str): The text to write to the file.
+    filename (str): The name of the file to write to.
+    text (str): The text to write to the file.
 
     Returns:
-        int: The number of characters written to the file.
-	"""
+    int: The number of characters written to the file.
+    """
 	with open(filename, 'w', encoding='utf-8') as file:
 		return file.write(text)
