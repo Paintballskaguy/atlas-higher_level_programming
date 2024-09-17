2bc02dbdd0bd1db53db9ad6b3cce336651310c95
Paintballskaguy <Johnstournament@gmail.com>
2024-09-16 23:01:47 -0500

    task 8 - importing the base from 7.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python3
 """
-This module defines a class Rectangle that inherits from BaseGeometry.
+This module defines a class Rectangle that inherits from BaseGeometry (7-base_geometry).
 """
 
-class BaseGeometry:
-    """A class with methods to calculate area and validate integers."""
-
-    def area(self):
-        """Raises an exception indicating that the area method is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates that value is a positive integer.
-
-        Args:
-            name (str): The name of the value (assumed to be a string).
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer or is a boolean.
-            ValueError: If value is less than or equal to 0.
-        """
-        if isinstance(value, bool) or not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
