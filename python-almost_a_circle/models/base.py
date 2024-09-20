f101d2df8a7fb0370230d33fa6159fee69b5a468
Paintballskaguy <Johnstournament@gmail.com>
2024-09-20 17:45:47 -0500

    task 1 and 2. square and rectangles

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the base class. it will manage 'id' attributes for all other classes.
+"""
+
+
+class Base:
+    """Base class manages the 'id' for all"""
+    __nb_objects = 0
+    
+    def __init__(self, id=none):
+        """Initialize a new Base instance.
+        
+        Args:
+            id (int): If provided, the instance's public id attribute is set to this value.
+                      Otherwise, __nb_objects is incremented and assigned to id.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+This is the module to define the rectangle.
+"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """ inherts from the parent in Base.py """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle instance.
+        
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x coordinate of the rectangle.
+            y (int): The y coordinate of the rectangle.
+            id (int): The id of the rectangle (optional).
+        """
+        super().__init__(id)
+        self.width = width 
+        self.height = height
+        self.x = x
+        self.y = y
+        
+@property
+def width(self)
+    return self.__width
+
+@width.setter
+def width(self, value):
+    if type(value) is not int:
+        raise TypeError("width must be an int")
+    if value <= 0:
+        raise ValueError("width must be > 0")
+    self.__width = value
+    
+@property
+def height(self):
+    return self.__height
+
+@height.setter
+def height(self, value):
+    if type(value) is not int:
+        raise TypeError("height must be an int")
+    if value <= 0:
+         raise ValueError("height must be > 0")
+    self.__height = value
+    
+@#property
+def x(self):
+    return self.__x
+    
