f767b8883c2d01e6aca752edd16ec39b52bbe7e1
Paintballskaguy <Johnstournament@gmail.com>
2024-09-21 23:49:53 -0500

    task 16 - base update.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -36,3 +36,21 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+        
+        Args:
+            list_objs (list): A list of instances that inherit from Base.
+        """
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+
+        if list_objs is not None:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+
+        json_string = cls.to_json_string(list_dicts)
+
+        with open(filename, "w") as f:
+            f.write(json_string)
