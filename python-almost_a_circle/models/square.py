c4ebb7a5285c71596af4312fc7069fa429983c27
Paintballskaguy <Johnstournament@gmail.com>
2024-09-21 22:50:44 -0500

    task 11 - propertyy and setter for the size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,6 +17,25 @@ class Square(Rectangle):
             id (int): The id of the Square (optional).
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter for size, returns width or height"""
+        return self.width
+    
+    @size.setter
+    def size(self, value)
+    """Setter for size. sets both width and height to same value
+        Args:
+            value (int): The size value to set for both width and height.
+        
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is <= 0.
+    """
+        self.width = value
+        self.height = value
+        
         
     def __str__(self):
         """override __str__ method to return [square]"""
