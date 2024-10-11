35ec5fe91ddec16894592299ef3f761dfb3a1288
Paintballskaguy <Johnstournament@gmail.com>
2024-10-11 16:36:43 -0500

    added '' to the create statment

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 -- Creates the table with columns id, name, score
 --puts 4 records into the table.
 
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS 'second_table' (
     id INT,
     name VARCHAR(256),
     score INT
