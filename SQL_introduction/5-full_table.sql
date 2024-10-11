3fb6dac74334026c3de81bf7b245f56f83f7b484
Paintballskaguy <Johnstournament@gmail.com>
2024-10-11 15:57:47 -0500

    task 5 -  simplified the script some. first one was overally complicated.

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- Script to print the description of the table first_table from the current database
 
-SELECT column_name, column_type, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_name = 'first_table'
-AND table_schema = DATABASE();
+
+SHOW CREATE TABLE 'first_table';
