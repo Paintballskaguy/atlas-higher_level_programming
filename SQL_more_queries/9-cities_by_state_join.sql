f109b64c0e1e001bc075591340ea27da517ddb7f
Paintballskaguy <Johnstournament@gmail.com>
2024-10-13 17:07:10 -0500

    task 9 - trying new verbage.

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Script to list all cities from the database.
 
-SELECT cities.id, cities.name
-FROM cities
-JOIN states ON cities.state_id = states.id
+SELECT cities.id, cities.name, states.name 
+FROM cities, states 
+WHERE cities.state_id = states.id 
 ORDER BY cities.id ASC;
