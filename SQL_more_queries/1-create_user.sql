a5c24b481c7f345e2ac744166be51f58c91053ec
Paintballskaguy <Johnstournament@gmail.com>
2024-10-13 16:11:24 -0500

    task 1 - create a user and checks to make sure it exists.

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,10 @@
+-- script to create a sql user with id user_0d_1
+
+-- makes user and checks that it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to new user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- makes sure the privileges is applied
+FLUSH PRIVILEGES;
