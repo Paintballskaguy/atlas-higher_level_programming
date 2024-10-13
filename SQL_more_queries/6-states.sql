cf68c3f8a084a00f76516b8aa4da3bb041b4897c
Paintballskaguy <Johnstournament@gmail.com>
2024-10-13 16:44:31 -0500

    silly issue. create database if,, not is.

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script to create a database hbtn_0d_usa and a table states
 
-CREATE DATABASE IS NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
