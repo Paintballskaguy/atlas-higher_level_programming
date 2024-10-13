e1e2cadfe949486684dea5b805540caf7bb1759c
Paintballskaguy <Johnstournament@gmail.com>
2024-10-13 17:17:16 -0500

    ADDED A JOIN STATEMENT.

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Script to list the shows in the database and have at least 2 genres
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows
+JOIN tv_show_genres WHERE tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
