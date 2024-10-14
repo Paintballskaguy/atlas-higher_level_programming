c9be3568f6023290e1bf61b9fbabb11229595317
Paintballskaguy <Johnstournament@gmail.com>
2024-10-14 12:33:03 -0500

    task 11 - got the sql server connected and able to test locally!!

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- script to list all the shows with their genre ID and display NULL
+USE hbtn_0d_tvshows;
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+ LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
