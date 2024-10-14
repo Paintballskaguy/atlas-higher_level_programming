d3e9f9d539e22d3d32c075443ec48da26587dcb9
Paintballskaguy <Johnstournament@gmail.com>
2024-10-14 15:06:33 -0500

    task 12  removed the order by

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Script to show all the shows in the database
 
-SELECT tv_shows.title
-FROM tv_shows
-WHERE tv_shows.id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC;
