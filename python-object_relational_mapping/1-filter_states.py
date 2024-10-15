79505b0cde93c6033280d7d048822130e14e0a39
Paintballskaguy <Johnstournament@gmail.com>
2024-10-15 17:56:41 -0500

    testing again.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,15 +10,17 @@ import MySQLdb
 from sys import argv
 
 
-
 def list_states_starting_with_n():
+    print("Connecting to database...")
     db = MySQLdb.connect(host="localhost",
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
                          port=3306)
-
+    print("Connected!")
+    
     cur = db.cursor()
+    print("Executing query...")
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%; ORDER by id ASC")
     rows = cur.fetchall()
     for row in rows:
@@ -26,7 +28,6 @@ def list_states_starting_with_n():
 
     cur.close()
     db.close()
-
 
     if __name__ == "__main__":
         list_states_starting_with_n()
