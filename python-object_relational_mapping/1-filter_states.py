ab809bf0a94377ea3cc49bfb0e168b6bd2780041
Paintballskaguy <Johnstournament@gmail.com>
2024-10-15 16:06:50 -0500

    task 1 - filtering all the states and only showing the states that start with N.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+This script lists all the states with a name starting with 'N'
+only from database hbtn_0e_0_usa
+
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def list_states_starting_with_n():
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%; ORDER by id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+    if __name__ == "__main__":
+        list_states_starting_with_n()
