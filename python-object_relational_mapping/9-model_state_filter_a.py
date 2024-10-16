74f208d7d808c40839c34b156e1f7a1d6254b0e2
Paintballskaguy <Johnstournament@gmail.com>
2024-10-15 20:02:49 -0500

    task 9 - lista ll the states that are in a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script to list all State objects that contain the letter a.
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+def filter_states_with_a():
+    """
+    Lists all State objects that contain the letter 'a', sorted by states.id in ascending order.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    filter_states_with_a()
