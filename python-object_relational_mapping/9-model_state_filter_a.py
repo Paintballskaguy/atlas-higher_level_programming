69a19376d9d5894a0622080d683c2b09ad3ae09d
Paintballskaguy <Johnstournament@gmail.com>
2024-10-15 20:04:43 -0500

    formatting

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,14 +14,19 @@ def filter_states_with_a():
     Lists all State objects that contain the letter 'a', sorted by states.id in ascending order.
     """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name.like('%a%')
+    ).order_by(State.id).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
