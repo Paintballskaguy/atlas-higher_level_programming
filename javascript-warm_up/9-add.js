bc35effdba4dd15da5aaab2ff340a6be019c1a81
Paintballskaguy <Johnstournament@gmail.com>
2024-11-14 18:47:48 -0600

    checking numbers out with || and NaN.

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+function add (a, b) {
+  return a + b;
+}
+
+const num1 = parseInt(args[0], 10);
+const num2 = parseInt(args[1], 10);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  console.log(add(num1, num2));
+}
